b8976d9e454932f061599bce29403a25280abfbc
Hazmi35 <contact@hzmi.xyz>
2020-09-02 12:22:56 +0700

    fix: fix eslint config on extending to base

--- typescript/index.js
+++ typescript/index.js
@@ -3,7 +3,7 @@
 const path = require("path");
 
 module.exports = {
-    extends: [path.join(__dirname, "..", "index.js"), path.join(require.resolve("@hazmi35/eslint-config/typescript"))],
+    extends: [path.join(__dirname, "..", "index.js"), path.join(require.resolve("@hazmi35/eslint-config/typescript")), path.join(__dirname, "..", "index.js")],
     rules: {
         "@typescript-eslint/no-var-requires": "off"
     }
